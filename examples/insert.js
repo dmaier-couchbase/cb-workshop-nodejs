08de96f809ae94bb6c9cf8c4dd46ad6004396ca1
David Maier <david.maier@couchbase.com>
2015-04-30 15:58:14 +0200

    Added a simple insert example

---
+++ examples/insert.js
@@ -0,0 +1,22 @@
+ouchbase = require('couchbase');
+var cluster = new couchbase.Cluster('couchbase://localhost');
+var bucket = cluster.openBucket('workshop', 'test');
+
+
+var user = {};
+user.uid = 'dmaier';
+user.first_name = 'David';
+user.last_name = 'Maier';
+user.address = 'Somewhere';
+user.email = 'david.maier@couchbase.com';
+
+bucket.insert('user::' + user.uid , user , function(err, res) {
+	  
+	  if (err) {
+		      console.log('operation failed', err);
+		          return;
+			    }
+
+	    console.log('success!', res);
+
+});
